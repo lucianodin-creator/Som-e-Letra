251ed503ed861892869b42e8532ffe013193c417
lucianodin-creator <lucianodin@gmail.com>
2026-03-06 12:14:07 -0300

    Versao Senior Final

---
+++ banco.js
@@ -0,0 +1,5 @@
+const minhasMusicas = [
+    { titulo: "Deixa a Vida Me Levar", artista: "Zeca Pagodinho", id: "9S-70L_S9Xo" },
+    { titulo: "Cheia de Manias", artista: "Raça Negra", id: "F0vX7C0f-rc" },
+    { titulo: "Verdade", artista: "Zeca Pagodinho", id: "tuZ9HnIn9_E" }
+];
